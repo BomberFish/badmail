dbef1c3ce3ad0237e4c65300fbebbbbfc9a53276
BomberFish <87151697+BomberFish@users.noreply.github.com>
2022-11-14 21:53:03 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# badmail
+literally the worst mail app. please don't use this. passwords are stored in plaintext so anyone who looks at the file can get access to your email to, say, send everyone you know crypto scams
+
+how to use (if you really want to)
+- make auth.txt with following contents:
+```"<your email>", <your password>"```
+(hint for gmail users: use app password)
+- profit????
---
+++ mail.py
@@ -0,0 +1,47 @@
+#importing pip like this will fail in the future, should be replaced.
+for n in range(1):
+        try:
+           import pip
+        except ImportError as missing:
+            print("You don't have pip. Exiting...")
+            exit()
+
+#install package function
+def install(package):
+    pip.main(['install', package])
+
+#mail module checker & install
+for n in range(1):
+        try:
+           import yagmail
+        except ImportError as missing:
+            if input("You don't have the yagmail module. Install it now? [Y/n]").lower() == "y":	
+               install(missing.name)
+               print("Please run the program again for changes to apply.")
+               exit()
+            else:
+               print("Abort.")
+               exit()
+
+
+auth = open("auth.txt", "r") 
+mail = yagmail.SMTP(auth.read())
+
+#TODO: multiple recipients
+recipient = str(input("enter recipient> "))
+
+subject = str(input("enter subject> "))
+
+#TODO: multiple lines
+content=str(input("enter text> "))
+
+#TODO: multiple attachmets
+doAttach=str(input("add an attachment? (y/N)> "))
+if doAttach == "y":
+   attachment=str(input("enter absolute or relative path to attachment> "))
+else:
+   attachment=0
+
+while True:
+  mail.send(to=recipient, subject=subject, contents=content, attachments=attachment)
+  print("sent!")
